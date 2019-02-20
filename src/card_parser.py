98b63a734d56d4eda19755e336c1b4af29d92d44
Scott Harris <47220742+NinjaPuff@users.noreply.github.com>
2019-02-20 18:39:45 -0500

    Edits to booster printing

--- src/card_parser.py
+++ src/card_parser.py
@@ -4,7 +4,10 @@ from mtgsdk import Card, Set, Type, Supertype, Subtype, Changelog
 # print('cardList size: ' + str(cardList.__len__()))
 # for card in cardList:
 #     print(card.name)
+print(Set.find('grn').booster)
 
 booster = Set.generate_booster('grn')
+print(len(booster))
+
 for card in booster:
     print(f'{card.name} Rarity: {card.rarity}')

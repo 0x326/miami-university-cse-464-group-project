0d684b5fb901bcaf079be398fe1650c7608bab35
Scott Harris <47220742+NinjaPuff@users.noreply.github.com>
2019-02-26 16:00:15 -0500

    Simplified Filewriting

--- src/card_parser.py
+++ src/card_parser.py
@@ -4,20 +4,17 @@ from mtgsdk import Card, Set, Type, Supertype, Subtype, Changelog
 # print('cardList size: ' + str(cardList.__len__()))
 # for card in cardList:
 #     print(card.name)
-print(Set.find('grn').booster)
 
-#booster = Set.generate_booster('grn')
-#print(len(booster))
+# booster = Set.generate_booster('grn')
+# print(len(booster))
 
-grn = Card.where(set = 'grn').where(language="English").all()
+grn = Card.where(set='grn').where(language="English").all()
 
-file_object = open("GuildsOfRavnica.txt","w")
+with open("GuildsOfRavnica.txt", "w") as file_object:
 
-for card in grn:
+    for card in grn:
 
-    if(card.name == "Plains"):
-        break
-    file_object.write(f'{card.name:^30} | {(card.mana_cost if (card.mana_cost is not None) else ""):^20} | {card.cmc:^5} | {card.type.replace("—", "-"):^40} | {card.rarity:^10} | \n')
-    #print(f'{card.name} Rarity: {card.rarity}')
-
-file_object.close()
+        if card.name == "Plains":
+            break
+        file_object.write(f'{card.name:<30} | {(card.mana_cost if (card.mana_cost is not None) else ""):<20} | {card.cmc:<5} | {card.type.replace("—", "-"):<40} | {card.rarity:<10} | \n')
+        # print(f'{card.name} Rarity: {card.rarity}')

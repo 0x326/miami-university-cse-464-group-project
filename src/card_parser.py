ea5228c7db447ce586a5974105f6c34f7e282082
Scott Harris <47220742+NinjaPuff@users.noreply.github.com>
2019-03-07 19:53:37 -0500

    Generated GRN cards

--- src/card_parser.py
+++ src/card_parser.py
@@ -8,13 +8,12 @@ from mtgsdk import Card, Set, Type, Supertype, Subtype, Changelog
 # booster = Set.generate_booster('grn')
 # print(len(booster))
 
-grn = Card.where(set='grn').where(language="English").all()
+grn = Card.where(set='rna').where(language="English").all()
 
-with open("GuildsOfRavnica.txt", "w") as file_object:
+with open("RavnicaAllegience.txt", "w") as file_object:
 
     for card in grn:
 
         if card.name == "Plains":
             break
-        file_object.write(f'{card.name:<30} | {(card.mana_cost if (card.mana_cost is not None) else ""):<20} | {card.cmc:<5} | {card.type.replace("—", "-"):<40} | {card.rarity:<10} | \n')
-        # print(f'{card.name} Rarity: {card.rarity}')
+        #file_object.write(f'{card.name:<30} | {(card.mana_cost if (card.mana_cost is not None) else ""):<20} | {card.cmc:<5} | {card.type.replace("—", "-"):<40} | {card.rarity:<10} | \n')

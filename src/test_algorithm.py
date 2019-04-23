8b691e7137078fd39ca0dac0bc0a63ad9036eedb
John Meyer <0x326@users.noreply.github.com>
2019-04-22 22:59:40 -0400

    Add sample test

---
+++ src/test_algorithm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from hypothesis import given
+from hypothesis.strategies import *
+
+from algorithm import generate_booster_packs
+
+
+@given(lists(text(), min_size=40))
+def test_generate_booster_packs(mtg_set):
+    deck = generate_booster_packs(mtg_set)
+    deck = tuple(deck)
+    assert len(deck) >= 40
+
+
+if __name__ == '__main__':
+    test_generate_booster_packs()

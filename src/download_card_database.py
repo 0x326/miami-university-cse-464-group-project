d0a265d9a7658ad26f238fc9a0519a5a6eacb1cf
John Meyer <0x326@users.noreply.github.com>
2019-04-28 00:09:10 -0400

    Query MTG database to get extra card info

--- src/download_card_database.py
+++ src/download_card_database.py
@@ -1,22 +1,92 @@
 #!/usr/bin/env python3
 
-from mtgsdk import Card, Set, Type, Supertype, Subtype, Changelog
+from pathlib import Path
+from typing import Mapping, Any
 
-# cardList = Card.where(set = 'grn').where(language="English").all()
-# print('cardList size: ' + str(cardList.__len__()))
-# for card in cardList:
-#     print(card.name)
+from mtgsdk import Card
+from yaml import safe_load, safe_dump
 
-# booster = Set.generate_booster('grn')
-# print(len(booster))
 
-grn = Card.where(set='rna', language='English')
-num: int = 1
-with open("RavnicaAllegienceCardIDs.txt", "w") as file_object:
+def generate_card_entry(card: Card) -> Mapping[str, Any]:
+    entry = {
+        'Name': card.name,
+        'Rarity': card.rarity,
+        'CMC': int(card.cmc),
+        'Mana cost': card.mana_cost,
+        'Image URL': card.image_url,
+    }
 
-    for card in grn:
+    card_type: str = card.types[0]
+    card_type = card_type.capitalize()
+    entry.update({
+        'Type': card_type,
+    })
 
-        if card.name == "Plains":
-            break
-        file_object.write(f'{num} \n')
-        num += 1
+    if 'Land' in card_type:
+        entry.update({
+            # Lands-only
+            'Land colors': card.color_identity,
+        })
+
+    elif 'Enchantment' in card_type:
+        pass
+
+    elif 'Artifact' in card_type:
+        pass
+
+    elif 'Planeswalker' in card_type:
+        pass
+
+    elif 'Creature' in card_type:
+        try:
+            entry.update({
+                'Creature power': int(card.power),
+            })
+        except ValueError:
+            pass
+
+        try:
+            entry.update({
+                'Creature toughness': int(card.toughness),
+            })
+        except ValueError:
+            pass
+
+    elif 'Sorcery' in card_type:
+        pass
+
+    elif 'Instant' in card_type:
+        pass
+
+    return entry
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Download a local copy of the MTG database')
+    parser.add_argument('set', metavar='SET_CODE', type=str,
+                        help='The code of the set to download')
+    parser.add_argument('--db-file', '-o', metavar='DATABASE_FILE', type=Path, default='card_db.yml',
+                        help='The code of the set to download')
+    args = parser.parse_args()
+
+    with open(args.db_file) as in_file:
+        card_db = safe_load(in_file)
+        try:
+            assert isinstance(card_db, dict)
+
+        except AssertionError:
+            card_db = {}
+
+    with open(args.db_file, 'w') as out_file:
+        mtg_set: str = args.set.upper()
+
+        card_db.update({
+            mtg_set: {
+                'Cards': {int(card.number): generate_card_entry(card)
+                          for card in Card.where(set=mtg_set, language='English').iter()},
+            },
+        })
+
+        safe_dump(card_db, out_file, indent=2, width=120)
